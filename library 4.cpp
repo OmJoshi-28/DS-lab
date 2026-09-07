7f7f052145e9627e3597255359894b4c13d7259f
Om Y. Joshi <om.aries2008@gmail.com>
2026-09-08 02:21:36 +0530

    crest library.cpp

---
+++ library 4.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int book[10];
+    int n = 0;
+    int choice;
+    int searchID;
+
+    do
+    {
+        cout << "\n\n===========SMART LIBRARY===========";
+        cout << "\n1. Add book:-";
+        cout << "\n2. Display book:-";
+        cout << "\n3. Search book:-";
+        cout << "\n4. Exit";
+        cout << "\nENTER YOUR CHOICE:- ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            cout << "Enter book ID:-";
+            cin >> book[n];
+            n++;
+            cout << "Book Added!!!";
+        }
+
+        else if (choice == 2)
+        {
+            cout << "\n Books in library:-\n";
+            for (int i = 0; i < n; i++)
+            {
+                cout << book[i] << endl;
+            }
+        }
+
+        else if (choice == 3)
+        {
+            cout << "Enter book ID to search:-";
+            cin >> searchID;
+            bool found = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (book[i] == searchID)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (found)
+            {
+                cout << "\nBook Found!!!";
+            }
+
+            else
+            {
+                cout << "\nBook Not Found:(";
+            }
+        }
+
+        else if (choice == 4)
+        {
+            cout << "Thank you!!!";
+        }
+
+        else
+        {
+            cout << " Invalid Choice";
+        }
+    } while (choice != 4);
+
+    return 0;
+}
