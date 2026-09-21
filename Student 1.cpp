bab8fbbd54e29410d07aa93371748f5ee5dacc08
Om Y. Joshi <om.aries2008@gmail.com>
2026-09-21 15:05:57 +0530

    Add program to input and display student roll numbers

---
+++ Student 1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rno1, rno2, rno3, rno4, rno5;
+
+    cout << "enter the roll no. of student 1:- ";
+    cin >> rno1;
+
+    cout << "enter the roll no. of student 2:- ";
+    cin >> rno2;
+
+    cout << "enter the roll no. of student 3:- ";
+    cin >> rno3;
+
+    cout << "enter the roll no. of student 4:- ";
+    cin >> rno4;
+
+    cout << "enter the roll no. of student 5:- ";
+    cin >> rno5;
+
+    cout << "\nstudent 1 roll no.:- " << rno1;
+    cout << "\nstudent 2 roll no.:- " << rno2;
+    cout << "\nstudent 3 roll no.:- " << rno3;
+    cout << "\nstudent 4 roll no.:- " << rno4;
+    cout << "\nstudent 5 roll no.:- " << rno5;
+
+    return 0;
+}
